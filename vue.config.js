9e7a42bf04fcc58e3405b0ecf034e6f8853f0205
dargall <dargall91@comcast.net>
2025-04-13 17:28:36 -0400

    Fix: Cors connection (again)

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 3000,
-    https: true,
     proxy: {
       '^/api/v1/': {
         target: `http://localhost:${process.env.VUE_APP_API_URL}/api/v1`,
